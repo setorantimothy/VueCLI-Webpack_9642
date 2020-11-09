73e04cece2afb5c9916cfab0039587f4ce1c922a
Eras <setorantimothy@gmail.com>
2020-11-09 12:39:56 +0700

    UGD Vue Basic

--- src/components/DashboardLayout.vue
+++ src/components/DashboardLayout.vue
@@ -39,6 +39,8 @@ export default {
             items: [
                 { title: "Dashboard", to: "/"},
                 { title: "GD", to: "/gd"},
+                { title: "UGD", to: "/ugd"},
+                { title: "TUGAS", to: "/tugas"},
             ],
         };
     },
--- src/router.js
+++ src/router.js
@@ -28,6 +28,16 @@ const router = new VueRouter({
                     name:'Guided',
                     component: importComponent('TodoList/List'),
                 },
+                {
+                    path: "/ugd",
+                    name:'Unguided',
+                    component: importComponent('TodoList/ListUGD'),
+                },
+                {
+                    path: "/tugas",
+                    name:'Tugas',
+                    component: importComponent('TodoList/ListTugas'),
+                },
             ]
         },
     ]
